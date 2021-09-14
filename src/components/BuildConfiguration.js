a2d02289e4c67e88feb83489092ebf0e9816c1f8
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2021-09-14 23:08:55 +0700

    Lam btvn buoi 13 reacjs router Api tao node server git rieng

---
+++ src/components/BuildConfiguration.js
@@ -0,0 +1,13 @@
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+// import './css/ .css'
+
+export function BuildConfiguration() {
+    return (
+        <>
+            <h1 className='container  text-center mt-5 '>Xây dựng cấu hình</h1>
+        </>
+    )
+}
